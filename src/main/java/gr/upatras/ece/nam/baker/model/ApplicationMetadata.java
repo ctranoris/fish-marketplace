6e53c58bafaccb86c3eaae89a24267d303b3665d
tranoris <tranoris@88d6be61-b049-4d1a-ac2d-38c3f0f8cafa>
2015-01-13 21:42:07 +0000

    changed post,put product API added Container and DeployArtifact in model

--- src/main/java/gr/upatras/ece/nam/baker/model/ApplicationMetadata.java
+++ src/main/java/gr/upatras/ece/nam/baker/model/ApplicationMetadata.java
@@ -15,25 +15,31 @@
 
 package gr.upatras.ece.nam.baker.model;
 
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Basic;
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
-import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 
 
 @Entity(name = "ApplicationMetadata")
 public class ApplicationMetadata extends Product{
 
 
+	@OneToMany(cascade = { CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
+	@JoinTable()
+	private List<Container> containers = new ArrayList<Container>();
 
+	public List<Container> getContainers() {
+		return containers;
+	}
+
+	public void setContainers(List<Container> containers) {
+		this.containers = containers;
+	}
+	
+	
 
 }
